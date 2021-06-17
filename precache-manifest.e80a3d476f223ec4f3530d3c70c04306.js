de18c1b907adcb438ae12049c409fa2333ad76c9
EkisWasTaken <elias.ekman@iths.se>
2021-06-17 18:56:12 +0200

    fixing

---
+++ precache-manifest.e80a3d476f223ec4f3530d3c70c04306.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68c29d3108dc99405bb",
+    "url": "/css/app.479255fd.css"
+  },
+  {
+    "revision": "ee9e98c8cd047219395d",
+    "url": "/css/chunk-vendors.e36598e4.css"
+  },
+  {
+    "revision": "cf2f89c88d8076a5a51ea5d2aecd0f15",
+    "url": "/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "621db650e91d011e30c3e3952bb35087",
+    "url": "/img/android-chrome-512x512.png"
+  },
+  {
+    "revision": "935abc28406e74279a22ec5802b009eb",
+    "url": "/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "cbe19108684ad12a0e52e6c23f985d8d",
+    "url": "/img/bmwcar.cbe19108.png"
+  },
+  {
+    "revision": "1018660ab9c60d87877ebe0cca6249bb",
+    "url": "/img/favicon-16x16.png"
+  },
+  {
+    "revision": "d4be1a0a6e16e9c1ae673bf52e5a786f",
+    "url": "/img/favicon-32x32.png"
+  },
+  {
+    "revision": "cd286a321863a38b01b65ab6ab38b4b2",
+    "url": "/img/logo.cd286a32.png"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/img/site.webmanifest"
+  },
+  {
+    "revision": "1fba621fd62f4119016fc639df4959f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d68c29d3108dc99405bb",
+    "url": "/js/app.9297d9d6.js"
+  },
+  {
+    "revision": "ee9e98c8cd047219395d",
+    "url": "/js/chunk-vendors.45774526.js"
+  },
+  {
+    "revision": "27a30e9a98bef9e1d276d4aff6e75e27",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
